3c0cfa586aff69e0cbb8837e85dc1c9ec053cb39
ellxor <ellxor@protonmail.ch>
2025-09-14 14:13:20 +0100

    Add GCC implementation of defer

--- stddefer.h
+++ stddefer.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#if !defined(__clang__) || !defined(__BLOCKS__)
+#if defined(__clang__)
+
+#if !defined(__BLOCKS__)
 #error Defer mechanism is only supported in clang using `-fblocks`. Note that with low optimizations \
 this may require linking with `-lBlocksRuntime`, but all blocks should be inlined with `-O2` or above.
 #include <fatalerror> // stop compilation at this point
@@ -15,3 +17,19 @@ this may require linking with `-lBlocksRuntime`, but all blocks should be inline
 static inline void __defer_handler(void (^*f)(void)) {
 	(*f)();
 }
+
+#endif
+
+
+#if defined(__GNUC__)
+
+#define __defer__(__defer_f,__defer_v) \
+        [[gnu::always_inline]] auto inline void __defer_f(int*);  \
+        [[gnu::cleanup(__defer_f)]] int __defer_v;         \
+        auto void __defer_f(int*)
+
+#define  __defer(i) __defer__(__defer_f ## i, __defer_v ## i)
+#define   _defer(i) __defer(i)
+#define    defer     _defer(__COUNTER__)
+
+#endif
